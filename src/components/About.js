8db659aaf4634b7153675fd8460dec5276c534b9
aleji0 <alex.springer@gmail.com>
2022-12-04 09:06:50 -0700

    it's going to have to do for now :/

---
+++ src/components/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Image from "../images/hero-bg.jpg"
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 className="top-title">Alex Springer</h2>
+            <hr></hr>
+            <div>
+                <img className="mb-5" id="avatar" alt="Alex Springer" />
+                
+                <p>
+                </p>
+                <p>
+                Blah blah, something here
+                </p>
+                <p>
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
